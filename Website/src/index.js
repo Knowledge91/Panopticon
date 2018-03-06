6042ee96f88000136f80971f62bff4baf068f716
knowledge91 <dirkhornung91@gmail.com>
2018-03-06 10:00:04 +0100

    minimal web3.eth.getBalance

--- Website/src/index.js
+++ Website/src/index.js
@@ -5,4 +5,9 @@ var web3 = new Web3('http://localhost:8545');
 
 var coinbase = web3.eth.coinbase;
 
-web3.eth.getAccounts().then(console.log);
+web3.eth.getAccounts().then((accounts) => {
+    const coinbase = accounts[0];
+    return web3.eth.getBalance(coinbase);
+}).then((balance) => {
+    document.getElementById("root").innerHTML = balance + " Ether";
+});
