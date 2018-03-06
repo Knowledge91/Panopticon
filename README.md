0efd6a16bd50f54d8c0637ccdfbb10967323d90b
knowledge91 <dirkhornung91@gmail.com>
2018-03-06 10:47:27 +0100

    minimal call Greeter

--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 
 ## Account
 - password: test
-- address: 0x59b89a799d0907c69b627a8cebaf92734ae2380c
-- contract address: 0xa9626f3b4ff5d530ef907fa560b67b9e9dd3a288
+- address: '0x59b89a799d0907c69b627a8cebaf92734ae2380c'
+- contract address: '0xa9626f3b4ff5d530ef907fa560b67b9e9dd3a288'
 
 ## Util
 create new Account
--- Website/src/index.js
+++ Website/src/index.js
@@ -3,7 +3,14 @@ console.log("yeah");
 
 var web3 = new Web3('http://localhost:8545');
 
-var coinbase = web3.eth.coinbase;
+var coinbase = '0x59b89a799d0907c69b627a8cebaf92734ae2380c';
+var greeterAdress = '0xa9626f3b4ff5d530ef907fa560b67b9e9dd3a288';
+
+var myContract = new web3.eth.Contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_greeting","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}], '0xa9626f3b4ff5d530ef907fa560b67b9e9dd3a288');
+
+myContract.methods.greet().call({from: '0x59b89a799d0907c69b627a8cebaf92734ae2380c'}).then((greeting) => {
+    console.log("yes", greeting);
+});
 
 web3.eth.getAccounts().then((accounts) => {
     const coinbase = accounts[0];
